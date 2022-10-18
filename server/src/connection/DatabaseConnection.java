ad1643c0336e982a13e342914b05021a9f98ad01
AYKDM <minhthai23012002@gmail.com>
2022-10-18 14:45:36 +0800

    abc

---
+++ server/src/connection/DatabaseConnection.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connection;
+
+/**
+ *
+ * @author ADMIN
+ */
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    private DatabaseConnection() {
+
+    }
+
+    public void connectToDatabase() throws SQLException {
+        String server = "localhost";
+        String port = "3306";
+        String database = "chat_application";
+        String userName = "root";
+        String password = "";
+        connection = java.sql.DriverManager.getConnection("jdbc:mysql://" + server + ":" + port + "/" + database, userName, password);
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
